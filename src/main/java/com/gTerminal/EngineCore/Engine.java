cb9b76fc3a8ac4e90d40c353c33eba3d4060a262
Guanlong Zhou <zhouguanlong2012@gmail.com>
2017-05-06 19:34:50 -0500

    Cleaned the project structure. Add Gradle Wrapper to the project. Added Gradle configuration.

---
+++ src/main/java/com/gTerminal/EngineCore/Engine.java
@@ -0,0 +1,11 @@
+package com.gTerminal.EngineCore;
+
+public class Engine {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello World! ");
+        for(String i: args)
+            System.out.print(i + " ");
+    }
+}
